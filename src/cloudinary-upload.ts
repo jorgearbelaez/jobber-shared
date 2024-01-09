bcb8921ca50963075073ae3a85b5238db39e4705
jorge arbelaez <jorge.a.arbelaez.c@gmail.com>
2024-01-08 22:49:15 -0500

    added helpers and github action

---
+++ src/cloudinary-upload.ts
@@ -0,0 +1,50 @@
+import cloudinary, { UploadApiErrorResponse, UploadApiResponse } from 'cloudinary';
+
+// this firts one for images
+export function uploads(
+  file: string,
+  public_id?: string,
+  overwrite?: boolean,
+  invalidate?: boolean
+): Promise<UploadApiResponse | UploadApiErrorResponse | undefined> {
+  return new Promise((resolve) => {
+    cloudinary.v2.uploader.upload(
+      file,
+      {
+        public_id,
+        overwrite,
+        invalidate,
+        resource_type: 'auto' // zip, images
+      },
+      (error: UploadApiErrorResponse | undefined, result: UploadApiResponse | undefined) => {
+        if (error) resolve(error);
+        resolve(result);
+      }
+    );
+  });
+}
+
+// this for videos 
+export function videoUpload(
+  file: string,
+  public_id?: string,
+  overwrite?: boolean,
+  invalidate?: boolean
+): Promise<UploadApiResponse | UploadApiErrorResponse | undefined> {
+  return new Promise((resolve) => {
+    cloudinary.v2.uploader.upload(
+      file,
+      {
+        public_id,
+        overwrite,
+        invalidate,
+        chunk_size: 50000,
+        resource_type: 'video'
+      },
+      (error: UploadApiErrorResponse | undefined, result: UploadApiResponse | undefined) => {
+        if (error) resolve(error);
+        resolve(result);
+      }
+    );
+  });
+}
